454eaa52041b33ea6586da52ad0568f913fffec2
Joe Sak <joe@joesak.com>
2021-12-10 19:16:09 -0600

    Update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,16 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.generators do |generate|
+    generate.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: false,
+      request_specs: true
+
+     generate.fixture_replacement :factory_bot, dir: "spec/factories"
+  end
 end
