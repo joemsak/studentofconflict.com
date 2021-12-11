5c583594fbde0bb437f955e43a53a5e8d2ea1d80
Joe Sak <joe@joesak.com>
2021-12-10 19:52:19 -0600

    Design a basic home page based on Tailwind UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def browser_title
+    if content_for?(:browser_title)
+      content_for(:browser_title)
+    elsif content_for?(:browser_title_prefix)
+      "#{content_for(:browser_title_prefix)} &bullet; #{t("site.title")}"
+    else
+      t("site.title")
+    end
+  end
 end
