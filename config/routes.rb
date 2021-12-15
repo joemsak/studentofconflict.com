5c44f468d49317b42d01136df5f46638c48f8383
Joe Sak <joe@joesak.com>
2021-12-14 19:31:25 -0600

    Add basic about page and update naming schemes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resource :home, only: :show
+  resource :home, only: :show, controller: :home
+  resource :about, only: :show, controller: :about
+
   resources :packages, only: [:show, :index]
+
   root "homes#show"
 end
